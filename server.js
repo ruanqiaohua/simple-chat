9b0f8c4e2cf9857b21a8570636b9987c0ab94cbc
ruanqiaohua <ruanqiaohua@hotmail.com>
2023-04-03 15:43:41 +0800

    index ui change

--- server.js
+++ server.js
@@ -2,27 +2,24 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mongoose = require('mongoose');
+// var io = require('socket.io')(http);
+// var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
-var Message = mongoose.model('Message',{
-  name : String,
-  message : String
-})
+// var Message = mongoose.model('Message',{
+//   name : String,
+//   message : String
+// })
 
-var dbUrl = 'mongodb://ruanqiaohua:62203957@mongo:27017/simple-chat'
-
-var fs = require("fs");
-var open_api_key = fs.readFileSync('key.text').toString(); 
+// var dbUrl = 'mongodb://ruanqiaohua:62203957@mongo:27017/simple-chat'
 
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: open_api_key,
+  apiKey: '',
 });
 const openai = new OpenAIApi(configuration);
 
@@ -44,27 +41,16 @@ app.get('/messages/:user', (req, res) => {
 app.post('/messages', async (req, res) => {
   
   try{
-    var message = new Message(req.body);
-
-    var savedMessage = await message.save()
-    console.log('saved');
-
-    var censored = await Message.findOne({message:'badword'});
-      if(censored)
-        await Message.remove({_id: censored.id})
-      else
-        io.emit('message', req.body);
-        // 回复消息
-        var content = req.body.message;
-        console.log(configuration.apiKey);
-        const completion = await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
-          messages: [{role: "user", content: content}],
-        });
-        var text = completion.data.choices[0].message.content;
-        io.emit('message', {'name':'ai','message':text});
+      // 回复消息
+      var content = req.body.message;
+      console.log(configuration.apiKey);
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: content}],
+      });
+      var text = completion.data.choices[0].message.content;
       // 返回
-      res.sendStatus(200);
+      res.sendStatus(200).send(text);
   }
   catch (error){
     res.sendStatus(500);
@@ -78,14 +64,14 @@ app.post('/messages', async (req, res) => {
 
 
 
-io.on('connection', () =>{
-  console.log('a user is connected')
-})
+// io.on('connection', () =>{
+//   console.log('a user is connected')
+// })
 
-mongoose.set('strictQuery', true);
-mongoose.connect(dbUrl ,(err) => {
-  console.log('mongodb connected',err);
-})
+// mongoose.set('strictQuery', true);
+// mongoose.connect(dbUrl ,(err) => {
+//   console.log('mongodb connected',err);
+// })
 
 var server = http.listen(80, () => {
   console.log('server is running on port', server.address().port);
